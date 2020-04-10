bdaa0b79007015c6ffefc38493f73f86ad137e76
Kevin <kevin.m.guertin@gmail.com>
2020-04-10 13:16:55 -0400

    minor changes

--- db/schema/02_dietary_restrictions.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS dietary_restrictions CASCADE;
-CREATE TABLE dietary_restrictions (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
-);
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,2 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (username, email, password) VALUES ('Bort', 'sampson@gmail.com', '12345');

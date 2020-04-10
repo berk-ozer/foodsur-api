3731eda162ca8cf103b300d8502c8511b3b5f42d
Kevin <kevin.m.guertin@gmail.com>
2020-04-10 16:17:14 -0400

    small server fix

--- server.js
+++ server.js
@@ -41,12 +41,10 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 

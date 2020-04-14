5402da4fe6957fa96d8e1642f0b71a2608387744
Berk Ozer <galipberkozer@gmail.com>
2020-04-13 22:22:37 -0400

    Send all dietary restriction data for a GET request to /api/restrictions

--- routes/dietaryRestrictions.js
+++ routes/dietaryRestrictions.js
@@ -6,7 +6,8 @@ const Dietary_restriction = require('../db/models/Dietary_restriction')
 module.exports = (db) => {
 
   router.get("/", async (req, res) => {
-
+    const restrictions = await Dietary_restriction(db).findAll();
+    res.send(restrictions);
   })
 
   return router;

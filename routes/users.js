50b8458af6a4db31299de6cc870c245076739fe6
Kevin <kevin.m.guertin@gmail.com>
2020-04-13 20:47:31 -0400

    add ability to check if user already exists in database

--- routes/users.js
+++ routes/users.js
@@ -11,12 +11,28 @@ const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const User = require('../db/models/User')
 
-
-
 module.exports = (db) => {
-  router.post("/new", (req, res) => {
-    const { username, email, password } = req.body;
-    User(db).create({username, email, password});
+
+  router.post("/new", async (req, res) => {
+    const {username, email, password} = req.body
+    const checkUser = await User(db).findAll({where: {
+      email
+    }})
+    if (checkUser.length === 0) {
+      await User(db).create({username, email, password})
+        .catch(err => console.log(err))
+      console.log('USER ADDED')
+    } else {
+      console.log('USER EXISTS')
+      res.status(400)
+    }
+
+
+    // const user = await User(db).findAll({where: {
+    //   email: 'bort'
+    // }})
+    // console.log(user.dataValues)
   })
+
   return router;
 };

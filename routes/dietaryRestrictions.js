fde422260a9f985b21bd0ffe7434c2d32a8a1979
Berk Ozer <galipberkozer@gmail.com>
2020-04-14 10:47:06 -0400

    Remove unneeded "require Sequelize" in dietaryRestriction route

--- routes/dietaryRestrictions.js
+++ routes/dietaryRestrictions.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Sequelize = require('sequelize');
 const Dietary_restriction = require('../db/models/Dietary_restriction')
 
 module.exports = (db) => {

5452d1f66f823add4f9117aed81bbf59e5fc7f76
Ata Ansari <ataansari@Atas-MacBook-Pro.local>
2020-04-14 22:14:13 -0400

    add route for adding favourites

--- db/models/Favourite.js
+++ db/models/Favourite.js
@@ -2,6 +2,11 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 module.exports = (db) => {
   const Favourite = db.define("favourite", {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false

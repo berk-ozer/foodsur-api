5dc69721793317e3cb166c54c7772759e502fbc6
Berk Ozer <galipberkozer@gmail.com>
2020-04-16 18:04:42 -0400

    Use environment variable in config.js for db port

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
-    "port": 5433
+    "port": process.env.DB_PORT
   },
   "test": {
     "username": "root",

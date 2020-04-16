f44503eae4a735a7d2fc15b1ac57cb8944f7a402
Berk Ozer <galipberkozer@gmail.com>
2020-04-16 13:29:08 -0400

    Fix the environment variables used in config.js

--- config/config.js
+++ config/config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 module.exports = {
   "development": {
     "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
     "operatorsAliases": false

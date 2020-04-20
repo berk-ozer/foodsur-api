31c120398cfb2dcedd8e9d6148478d959ad89612
Kevin <kevin.m.guertin@gmail.com>
2020-04-20 12:26:41 -0400

    add columns to favourite table for macros

--- models/favourite.js
+++ models/favourite.js
@@ -2,7 +2,11 @@
 module.exports = (sequelize, DataTypes) => {
   const Favourite = sequelize.define('Favourite', {
     apiId: DataTypes.STRING,
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    calories: DataTypes.DECIMAL(10, 2),
+    carbs: DataTypes.DECIMAL(10, 2),
+    protein: DataTypes.DECIMAL(10, 2),
+    fat: DataTypes.DECIMAL(10, 2)
   }, {});
   Favourite.associate = function (models) {
     Favourite.belongsToMany(models.User, {
@@ -10,11 +14,11 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: 'favouriteId',
       otherKey: 'userId'
     }),
-    Favourite.belongsToMany(models.DietaryRestriction, {
-      through: 'FavouriteDietaryRestrictions',
-      foreignKey: 'favouriteId',
-      otherKey: 'dietaryRestrictionId'
-    })
+      Favourite.belongsToMany(models.DietaryRestriction, {
+        through: 'FavouriteDietaryRestrictions',
+        foreignKey: 'favouriteId',
+        otherKey: 'dietaryRestrictionId'
+      })
   };
   return Favourite;
 };
